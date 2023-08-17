8e1f56094f61b0489770208b8b8a9328e73dfb8e
Dhivakar075 <diva.march2010@gmail.com>
2023-08-17 11:38:30 +0530

    rupee button and secure button xpath

--- src/main/java/com/NykaaIPT_Aug_TestNG/pom/PaymentPage.java
+++ src/main/java/com/NykaaIPT_Aug_TestNG/pom/PaymentPage.java
@@ -2,15 +2,8 @@ package com.NykaaIPT_Aug_TestNG.pom;
 
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
-import com.NykaaIPT_Aug_TestNG.base.BaseClass;
-
-public class Payment_Page extends BaseClass{
-	
-	public Payment_Page() {
-		PageFactory.initElements(driver, this);
-	}
+public class PaymentPage {
 
 	@FindBy(xpath = Payment.rupee_btn_xpath )
 	private WebElement rupee_btn_xpath;
