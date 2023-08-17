41046604ae99fb6f4010bcad944990bac5735def
gayathrithanikachalam <gayathrithanikachalam52@gmail.com>
2023-08-17 11:07:54 +0530

    loginpage

---
+++ src/main/java/com/NykaaIPT_Aug_TestNG/pom/LoginPage.java
@@ -0,0 +1,30 @@
+package com.NykaaIPT_Aug_TestNG.pom;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.NykaaIPT_Aug_TestNG.base.BaseClass;
+
+public class LoginPage extends BaseClass implements Login {
+	
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+	
+
+	@FindBy(xpath = "email_mobile_xpath")
+	private WebElement email_btn;
+	
+	public WebElement getEmail_btn() {
+		return email_btn;
+	}
+	
+	@FindBy(xpath = "signIn_proceed_btn_xpath")
+	private WebElement Sign_proceed;
+	
+	public WebElement getSign_proceed() {
+		return Sign_proceed;
+	}
+
+}
